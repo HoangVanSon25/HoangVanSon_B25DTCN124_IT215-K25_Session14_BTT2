15534a29b47e45dca390f2f6237e1f272913d8c5
HoangVanSon25 <167545691+HoangVanSon25@users.noreply.github.com>
2026-07-14 00:04:25 +0700

    Delete app directory

--- app/main.py
+++
@@ -1,6 +0,0 @@
-from fastapi import FastAPI
-from routers.student import router
-from database import Base, enginne
-Base.metadata.create_all(bind=enginne)
-app = FastAPI()
-app.include_router(router)
